996166386bc62195adf5f74380cbac333928b711
Florian Czarnecki <Florian.Czarnecki@adesso.de>
2021-06-18 14:54:59 +0200

    Add missing function argument, and attributes in UpdatePersonUseCase

--- budgeteer-core/src/main/java/de/adesso/budgeteer/core/person/port/in/UpdatePersonUseCase.java
+++ budgeteer-core/src/main/java/de/adesso/budgeteer/core/person/port/in/UpdatePersonUseCase.java
@@ -1,15 +1,20 @@
 package de.adesso.budgeteer.core.person.port.in;
 
+import de.adesso.budgeteer.core.person.domain.PersonWithRate;
 import lombok.Value;
 import org.joda.money.Money;
 
+import java.util.List;
+
 public interface UpdatePersonUseCase {
-    void updatePerson();
+    void updatePerson(UpdatePersonCommand command);
 
     @Value
     class UpdatePersonCommand {
+        long id;
         String name;
-        String internalNumber;
+        String importKey;
         Money defaultDailyRate;
+        List<PersonWithRate.Rate> rates;
     }
 }
